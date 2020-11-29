511dc5bfa6be88304e55bdb95c7989ce29f6ffe3
codgician <codgician@users.noreply.github.com>
2020-11-29 18:24:13 +0800

    minor refactor

---
+++ ssmgr-manager/hack.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# Error level: debug -> error
+sed -i "s/debug/error/g" \
+/usr/lib/node_modules/shadowsocks-manager/init/log.js
+
+# Disable verbose mode and enable ipv6 support
+sed -i "s/'-v'/'-s', '0.0.0.0', '-s', '::'/g"  \
+/usr/lib/node_modules/shadowsocks-manager/init/runShadowsocks.js
